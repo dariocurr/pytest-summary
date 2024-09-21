5248fa952f1b6d0800bf3c00a400d96f5028f419
Dario Curreri <48800335+dariocurr@users.noreply.github.com>
2024-09-21 15:31:35 +0200

    fix: 💪 Add workflow to automatize release #18 (#22)

---
+++ noxfile.py
@@ -0,0 +1,49 @@
+import nox
+import yaml
+
+
+@nox.session
+def update_tags(session: nox.Session):
+    session.run(
+        "git",
+        "config",
+        "user.name",
+        "github-actions",
+        external=True,
+        silent=False,
+    )
+    session.run(
+        "git",
+        "config",
+        "user.email",
+        "github-actions@users.noreply.github.com",
+        external=True,
+        silent=False,
+    )
+    with open(file="action.yml") as stream:
+        action = yaml.safe_load(stream=stream)
+    action = action["runs"]["steps"][2]["uses"]
+    version = action.split("@")[1]
+    major = version.split(".")[0]
+    for tag in [version, major]:
+        session.run(
+            "git",
+            "tag",
+            "--annotate",
+            "--force",
+            "--message",
+            "",
+            tag,
+            external=True,
+        )
+        session.run(
+            "git",
+            "push",
+            "--force",
+            "origin",
+            tag,
+            external=True,
+        )
+    output_file = session.env["GITHUB_OUTPUT"]
+    with open(file=output_file, mode="a") as f:
+        f.write(f"tag={version}\n")
